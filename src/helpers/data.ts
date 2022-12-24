2ce89f7a5501e3973f2d6277fd94188b17aa759e
Bouncing <bouncing@me.com>
2022-12-24 23:33:10 +0800

    feat:处理响应 data

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,6 +1,6 @@
 /*
  * @Date: 2022-12-24 09:18:02
- * @LastEditTime: 2022-12-24 09:24:49
+ * @LastEditTime: 2022-12-24 23:24:35
  * @Description:
  *
  * Copyright (c) 2022 by 课书壬盘 kebook.net, All Rights Reserved.
@@ -10,6 +10,17 @@ import { isPlainObject } from './util'
 export function transformRequest(data: any): any {
   if (isPlainObject(data)) {
     return JSON.stringify(data)
+  }
+  return data
+}
+
+export function transformResponse(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (error) {
+      // do nothing
+    }
   }
   return data
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 /*
  * @Author: 小神仙他爹老神经
  * @Date: 2022-11-27 14:24:26
- * @LastEditTime: 2022-12-24 17:21:09
+ * @LastEditTime: 2022-12-24 23:30:16
  * @Description: axios 入口文件
  *
  * Copyright (c) 2022 by 课书壬盘 kebook.net, All Rights Reserved.
  */
-import {AxiosRequestConfig,AxiosPromise} from './type'
+import {AxiosRequestConfig,AxiosPromise, AxiosResponse} from './type'
 import xhr from './xhr';
 import { buildURL } from './helpers/url';
-import { transformRequest } from './helpers/data';
+import { transformRequest, transformResponse } from './helpers/data';
 import { processHeaders } from './helpers/headers';
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then((res) => {
+    return transformResponseData(res);
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void{
@@ -35,6 +37,11 @@ function transformRequestData(config: AxiosRequestConfig): any{
 function transformHeaders(config: AxiosRequestConfig): any{
   const {headers={},data}=config
   return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse{
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios
